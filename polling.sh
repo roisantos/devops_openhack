f4a53ded423bf9138f602065ac28f42b665f5503
Juan Francisco Salamanca Carmona <JuanFryS@users.noreply.github.com>
2021-06-08 14:29:57 +0200

    Update polling.sh

--- polling.sh
+++ polling.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
 declare -i duration=1
+declare -i iterator=0
+declare -i max_iterations=10
 declare hasUrl=""
 declare endpoint
 
 usage() {
     cat <<END
     polling.sh [-i] [-h] endpoint
-    
+
     Report the health status of the endpoint
     -i: include Uri for the format
     -h: help
 END
 }
 
-while getopts "ih" opt; do 
-  case $opt in 
+while getopts "ih" opt; do
+  case $opt in
     i)
       hasUrl=true
       ;;
-    h) 
+    h)
       usage
       exit 0
       ;;
@@ -37,8 +39,8 @@ if [[ $1 ]]; then
 else
   echo "Please specify the endpoint."
   usage
-  exit 1 
-fi 
+  exit 1
+fi
 
 
 healthcheck() {
@@ -48,18 +50,25 @@ healthcheck() {
 }
 
 while [[ true ]]; do
-   result=`healthcheck $endpoint` 
+   result=`healthcheck $endpoint`
    declare status
-   if [[ -z $result ]]; then 
+   if [[ -z $result ]]; then
       status="N/A"
    else
-      status=${result:9:3}
-   fi 
+      status=${result:7:3}
+   fi
    timestamp=$(date "+%Y%m%d-%H%M%S")
    if [[ -z $hasUrl ]]; then
      echo "$timestamp | $status "
    else
-     echo "$timestamp | $status | $endpoint " 
-   fi 
+     echo "$timestamp | $status | $endpoint "
+   fi
+   if [ "$status" == "200" ]; then
+     exit 0
+   fi
    sleep $duration
+   iterator=$((iterator+1))
+   if [ "$iterator" == "$max_iterations" ]; then
+     exit 1
+   fi
 done
